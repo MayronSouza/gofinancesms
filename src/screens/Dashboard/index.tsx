1f2f77cb04b28754115aea143c1daf9020f48577
MayronSouza <mayron.souza@gmz.solutions>
2021-06-03 15:09:00 -0300

    creating the styles file of the Dashboard screen

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Container, Title } from './styles';
+
+
 
 const Dashboard = () => {
   return (
-    <View>
-      <Text>Dashboard</Text>
-    </View>
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
   );
 };
 
